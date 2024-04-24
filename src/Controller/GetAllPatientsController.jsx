6531fe5ee6c85f59d00879ac5a37a8bba34dbb0a
Thomas6A <thomas.baudrin@outlook.fr>
2024-04-24 15:40:37 +0200

    feat: add list all patients

--- src/Controller/GetAllPatientsController.jsx
+++ src/Controller/GetAllPatientsController.jsx
@@ -1,9 +1,38 @@
-import { Button, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import PatientService from "../Service/PatientService";
+import PatientModel from "../Model/PatientModel";
+import GetPatientsView from "../Vue/Pages/GetPatientsView";
 
 const GetAllPatientsController = () => {
-        return <Container>
-        <Button variant="secondary" >ajouter produit</Button>
-    </Container>
+    const [patients, setPatients] = useState([]);
+
+    useEffect(() => {
+        fetchPatients();
+    }, [])
+
+    const fetchPatients = async () => {
+        try {
+            let response = await PatientService.fetchPatients();
+            const listPatients = [];
+            response.data.forEach(data => {
+                let patient = new PatientModel();
+                patient.setId(data.idPatient);
+                patient.setPrenom(data.firstName);
+                patient.setNom(data.lastName);
+                patient.setSecu(data.socialSecurityNumber);
+                patient.setAnniversaire(data.birthdate);
+                patient.setCreation(data.createdAt);
+                patient.setModification(data.modifiedAt);
+                listPatients.push(patient);
+            });
+            setPatients(listPatients);
+        } catch (error) {
+            console.log("erreur" + error);
+        }
+    }
+
+
+    return (GetPatientsView(patients))
 
 }
 
--- src/Vue/Pages/GetPatientsView.jsx
+++ src/Vue/Pages/GetPatientsView.jsx
@@ -1,12 +1,32 @@
-import { Button, Container } from "react-bootstrap";
+import { Table } from "react-bootstrap";
+import { EyeFill, PencilFill, XOctagonFill } from "react-bootstrap-icons";
 
-const GetProducts = () => {
+const GetProducts = (patients) => {
 
 
     return (
-        <Container>
-            <Button variant="secondary" >ajouter produit</Button>
-        </Container>
+        <Table bordered>
+            <thead>
+                <tr>
+                    <th>Prénom</th>
+                    <th>Nom</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {patients.map(patient => (
+                    <tr key={patient.id}>
+                        <th>{patient.getPrenom()}</th>
+                        <th>{patient.getNom()}</th>
+                        <th>
+                            <EyeFill color="#17A2B8" className="mx-2"/>
+                            <PencilFill color="#FFC107" className="mx-2"/>
+                            <XOctagonFill color="#DC3545" className="mx-2"/>
+                        </th>
+                    </tr>
+                ))}
+            </tbody>
+      </Table>
     );
 };
 
